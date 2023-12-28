175be7dc9c1295f6ec388655e90997099bfd52de
alipoureskandar <alipoureskandar@outlook.com>
2023-12-28 14:23:05 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TipCalculator />
     </div>
   );
 }
 
-export default App;
+function TipCalculator() {
+  const [bill, setBill] = useState(0);
+  const [tip1, setTip1] = useState(0);
+  const [tip2, setTip2] = useState(0);
+
+  function handleReset() {
+    setBill(0);
+    setTip1(0);
+    setTip2(0);
+  }
+
+  const totalTip = (bill * (tip1 + tip2)) / 2 / 100;
+
+  return (
+    <div>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage tip={tip1} onSelect={setTip1}>
+        How did you like the food?
+      </SelectPercentage>
+      <SelectPercentage tip={tip2} onSelect={setTip2}>
+        How did your friend like it?
+      </SelectPercentage>
+      {bill > 0 ? (
+        <>
+          <Output bill={bill} totalTip={totalTip} />
+          <Reset onHandleReset={handleReset} />
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+function BillInput({ bill, onSetBill }) {
+  return (
+    <div>
+      <label>How much was the bill?</label>{" "}
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+function SelectPercentage({ children, tip, onSelect }) {
+  return (
+    <div>
+      <label>{children}</label>{" "}
+      <select value={tip} onChange={(e) => onSelect(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was ok (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">It was Amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ bill, totalTip }) {
+  return (
+    <h3>
+      You pay ${bill + totalTip} (${bill} + ${totalTip} tip)
+    </h3>
+  );
+}
+
+function Reset({ onHandleReset }) {
+  return <button onClick={onHandleReset}>Reset</button>;
+}
